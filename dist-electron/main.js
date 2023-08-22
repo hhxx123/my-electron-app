4b0107a98434074b7ef344c1e4fc4c72fb661739
liuying <liuying@jujin8.com>
2023-08-22 15:41:35 +0800

    集成electron 可正常启动

---
+++ dist-electron/main.js
@@ -0,0 +1,19 @@
+"use strict";
+const { app, BrowserWindow } = require("electron");
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      devTools: true,
+      // 集成网页和 Node.js，也就是在渲染进程中，可以调用 Node.js 方法
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+  win.loadURL(`${process.env["VITE_DEV_SERVER_URL"]}`);
+  win.webContents.openDevTools();
+};
+app.whenReady().then(() => {
+  createWindow();
+});
